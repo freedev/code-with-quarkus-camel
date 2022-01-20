a870bf5196a4decb4897a0d75f0a13a8b85dc5a5
Vincenzo D'Amore <v.damore@gmail.com>
2022-01-20 11:16:27 +0100

    first import

---
+++ src/main/java/it/damore/quarkus/azure/MyReactiveMessagingApplication.java
@@ -0,0 +1,18 @@
+package it.damore.quarkus.azure;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.concurrent.CompletionStage;
+
+@ApplicationScoped
+public class MyReactiveMessagingApplication {
+
+    @Incoming("source-in")
+    public CompletionStage<Void> consume(Message<String> message) {
+        System.out.println(message);
+        return message.ack();
+    }
+
+}
